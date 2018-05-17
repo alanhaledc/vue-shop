b67d92d909c4c003f917ebdb93e27291828cf50a
Hale Deng <haledeng88@gmail.com>
2018-05-17 21:26:28 +0800

    add vue-lazyload axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import LazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$axios = axios
+
+Vue.use(LazyLoad, {
+  loading: '/static/loading/loading-bars.svg'
+})
 
 /* eslint-disable no-new */
 new Vue({
