463cdf189d064800c41eb2b0855f0055759acc80
Hale Deng <haledeng88@gmail.com>
2018-05-18 23:33:39 +0800

    update router

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,55 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const Users = require('../models/users')
 
-module.exports = router;
+router.get('/', (req, res, next) => {
+  res.send('respond with a resource')
+})
+
+// 登录账户
+router.post('/login', (req, res, next) => {
+  let params = {
+    userName: req.body.userName,
+    userPwd: req.body.userPwd
+  }
+
+  Users.findOne(params, (err, doc) => {
+    if (err) {
+      res.json({
+        status: 1,
+        msg: err.message
+      })
+    } else {
+      if (doc) {
+        res.cookie('userId', doc.userId, {
+          path: '/',
+          maxAge: 1000 * 60 * 60 * 24 * 7
+        })
+        // req.session.user = doc
+        res.json({
+          status: 0,
+          msg: '',
+          results: {
+            userName: doc.userName
+          }
+        })
+      }
+    }
+  })
+})
+
+// 登出账户
+router.post('/logout', (req, res, next) => {
+  res.cookie('userId', '', {
+    path: '/',
+    maxAge: -1
+  })
+  // res.clearCookie('userId')
+  res.json({
+    status: 0,
+    msg: '',
+    results: ''
+  })
+})
+module.exports = router
