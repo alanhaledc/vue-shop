4d1cc53887952c7cf6e72d186b516177b340e034
Hale Deng <haledeng88@gmail.com>
2018-05-18 23:37:17 +0800

    add infiniteScroll package

--- src/main.js
+++ src/main.js
@@ -3,11 +3,18 @@ import App from './App'
 import router from './router'
 import axios from 'axios'
 import LazyLoad from 'vue-lazyload'
+import infiniteScroll from 'vue-infinite-scroll'
+
+import './assets/css/base.css'
+import './assets/css/product.css'
+import './assets/css/checkout.css'
+import './assets/css/login.css'
 
 Vue.config.productionTip = false
 
 Vue.prototype.$axios = axios
 
+Vue.use(infiniteScroll)
 Vue.use(LazyLoad, {
   loading: '/static/loading/loading-bars.svg'
 })
