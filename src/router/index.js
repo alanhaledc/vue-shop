e18751c2f67bfdaa0bbdc266311698240f3e5b6e
Hale Deng <haledeng88@gmail.com>
2018-05-21 23:25:17 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GoodList from 'pages/good-list'
+import Cart from 'pages/cart'
+import Address from 'pages/address'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'GoodList',
       component: GoodList
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
+    },
+    {
+      path: '/address',
+      name: 'Address',
+      component: Address
     }
   ]
 })
