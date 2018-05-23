d1af892d5ec5cd63ee9638a5ccb490c90596c381
Hale Deng <haledeng88@gmail.com>
2018-05-23 18:49:14 +0800

    update

--- server/app.js
+++ server/app.js
@@ -21,8 +21,8 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
-// app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'views')))
+app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'views')))
 
 // 未登录拦截
 app.use((req, res, next) => {
