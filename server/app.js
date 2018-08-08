67f60237dc7a6e60efeba7f9ab341cc24dc0a94f
Hale Deng <haledeng88@gmail.com>
2018-08-08 23:42:10 +0800

    add disable prevent url '/user/register'

--- server/app.js
+++ server/app.js
@@ -19,7 +19,8 @@ app.use(async (ctx, next) => {
     if (ctx.cookies.get('userId')) {
       await next()
     } else {
-      if (ctx.url === '/user/login' || ctx.url === '/user/logout' || ctx.url.indexOf('/goods/list') > -1) {
+      if (ctx.url === '/user/login' || ctx.url === '/user/logout' ||
+        ctx.url === '/user/register' || ctx.url.indexOf('/goods/list') > -1) {
         await next()
       } else {
         ctx.body = failResponse('当前未登录')
