561ff44d9c7e87e1e6ef93d9650deae0bac46417
Hale Deng <haledeng88@gmail.com>
2018-08-08 23:45:46 +0800

    add router guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import {Cookies} from 'quasar'
 
 import routes from './routes'
 
@@ -12,7 +13,7 @@ Vue.use(VueRouter)
 
 export default function (/* { store, ssrContext } */) {
   const Router = new VueRouter({
-    scrollBehavior: () => ({ y: 0 }),
+    scrollBehavior: () => ({y: 0}),
     routes,
 
     // Leave these as is and change from quasar.conf.js instead!
@@ -21,5 +22,16 @@ export default function (/* { store, ssrContext } */) {
     base: process.env.VUE_ROUTER_BASE
   })
 
+  Router.beforeEach((to, from, next) => {
+    if (!Cookies.get('userId')) {
+      if (to.name === 'goods') {
+        next()
+      } else {
+        next('/home/goods')
+      }
+    } else {
+      next()
+    }
+  })
   return Router
 }
