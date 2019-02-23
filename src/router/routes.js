8e7cdf1139af0f93d16c6978867aa54ef3e88461
Hale Deng <haledeng88@gmail.com>
2019-02-23 20:56:10 +0800

    remove home component name

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,13 +5,20 @@ const routes = [
   },
   {
     path: '/home',
-    name: 'home',
     component: () => import('layouts/MyLayout.vue'),
     children: [
-      {path: '/', redirect: 'goods'},
-      {path: 'goods', name: 'goods', component: () => import('pages/goods.vue')},
-      {path: 'cart', name: 'cart', component: () => import('pages/cart.vue')},
-      {path: 'trade', name: 'trade', component: () => import('pages/trade.vue')}
+      { path: '/', redirect: 'goods' },
+      {
+        path: 'goods',
+        name: 'goods',
+        component: () => import('pages/goods.vue')
+      },
+      { path: 'cart', name: 'cart', component: () => import('pages/cart.vue') },
+      {
+        path: 'trade',
+        name: 'trade',
+        component: () => import('pages/trade.vue')
+      }
     ]
   }
 ]
